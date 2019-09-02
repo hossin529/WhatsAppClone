25917cb6a526704eecd4834083f7b757cb418884
hoss dev <devhoss529@gmail.com>
2019-09-02 15:34:03 +0100

    first commit

---
+++ lib/ChatScreen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        conversation(3),
+        conversation(0),
+        conversation(0),
+        conversation(4),
+      ],
+      
+    );
+  }
+}
+
+Widget conversation(int messagesCount){
+  return  Container(
+    
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: ClipOval(
+                  child: Image.network('https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Name ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 21),),
+                        Text('message dsfsdf rtedt',style: TextStyle(color: Colors.grey),),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        SizedBox(height: 5,),
+                        Text('11:48 AM',style: TextStyle(color:  messagesCount>0 ?Color(0xFF25D366):Colors.grey),),
+                      messagesCount>0 ?  Chip(
+                         backgroundColor: Color(0xFF25D366),
+                         label: Text('$messagesCount',style: TextStyle(color: Colors.white),),
+                       ):Text(''),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: 330,
+                height: 1,
+                margin: EdgeInsets.only(left: 56,top: 21),
+                color: Colors.grey,
+              )
+            ],
+          ),
+        );
+}
---
+++ lib/Home.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/ChatScreen.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF128C7E),
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            indicatorWeight: 4,
+            labelStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            tabs: [
+              Tab(child: Icon(Icons.camera_alt)),
+              Tab(
+                text: 'CHATS',
+              ),
+              Tab(
+                text: 'STATUSES',
+              ),
+              Tab(
+                text: 'CALLS',
+              ),
+            ],
+          ),
+          title: Text('Whatsapp',style: TextStyle(fontSize: 21),),
+          actions: <Widget>[
+            Icon(Icons.search),
+            SizedBox(
+              width: 15,
+            ),
+            Icon(Icons.more_vert),
+            SizedBox(
+              width: 5,
+            ),
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            ChatScreen(),
+            Icon(Icons.directions_bike),
+            Icon(Icons.directions_bike),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.comment),
+          backgroundColor: Color(0xFF25D366),
+          onPressed: (){},
+        ),
+      ),
+    );
+  }
+}
