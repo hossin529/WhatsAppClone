b170e3be5cd725de0dd1fab7294e31f7e6240f3d
hoss dev <devhoss529@gmail.com>
2019-09-06 11:21:26 +0100

    format document

--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -10,52 +10,70 @@ class ChatScreen extends StatelessWidget {
         conversation(0),
         conversation(4),
       ],
-      
     );
   }
 }
 
-Widget conversation(int messagesCount){
-  return  Container(
-    
-          padding: EdgeInsets.all(8),
-          child: Column(
+Widget conversation(int messagesCount) {
+  return Container(
+    padding: EdgeInsets.all(8),
+    child: Column(
+      children: <Widget>[
+        ListTile(
+          leading: ClipOval(
+            child: Image.network(
+                'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              ListTile(
-                leading: ClipOval(
-                  child: Image.network('https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Name ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 21),),
-                        Text('message dsfsdf rtedt',style: TextStyle(color: Colors.grey),),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: <Widget>[
-                        SizedBox(height: 5,),
-                        Text('11:48 AM',style: TextStyle(color:  messagesCount>0 ?Color(0xFF25D366):Colors.grey),),
-                      messagesCount>0 ?  Chip(
-                         backgroundColor: Color(0xFF25D366),
-                         label: Text('$messagesCount',style: TextStyle(color: Colors.white),),
-                       ):Text(''),
-                      ],
-                    )
-                  ],
-                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Name ',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
+                  ),
+                  Text(
+                    'message dsfsdf rtedt',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
               ),
-              Container(
-                width: 330,
-                height: 1,
-                margin: EdgeInsets.only(left: 56,top: 21),
-                color: Colors.grey,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '11:48 AM',
+                    style: TextStyle(
+                        color: messagesCount > 0
+                            ? Color(0xFF25D366)
+                            : Colors.grey),
+                  ),
+                  messagesCount > 0
+                      ? Chip(
+                          backgroundColor: Color(0xFF25D366),
+                          label: Text(
+                            '$messagesCount',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        )
+                      : Text(''),
+                ],
               )
             ],
           ),
-        );
+        ),
+        Container(
+          width: 330,
+          height: 1,
+          margin: EdgeInsets.only(left: 56, top: 21),
+          color: Colors.grey.withOpacity(.2),
+        )
+      ],
+    ),
+  );
 }
